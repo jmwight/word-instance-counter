234302ec61430c9331660b1814641d6c55026503
John Morgan Wight <john.morgan.wight@proton.me>
2025-04-27 02:50:49 -0400

    FINISH MAIN
    finished main but untested

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "getword.h"
+
+#define MXWLEN	50 /* 99.999% of all words are <= 50 characters */
+#define TRUE	1u /* unsigned */
+#define FALSE	0u
 
 struct wnode *addword(struct wnode *wn, char *w)
 void fillwnodelist(struct **wnodelist, int wnodelistlen)
@@ -16,6 +21,50 @@ struct wnode
 int main(void)
 {
 	/* TODO: basic layout for function */
+	struct attr wattr;
+	char w[MAXWLEN + 1]; /* + 1 for the trailing \0 character at the end of
+				strings */
+	struct wnode *root = NULL;
+	unsigned int nw, uniquewcnt, totalwcnt;
+	nw = uniquewcnt = totalwcnt = 0;
+
+	/* get all words, put into tree, and get counts of unique words and
+	 * total words */
+	while((wattr = getword(w, MXWLEN + 1)) != EOF)
+	{
+		root = addword(root, w, &nw);
+		totalwcnt++;
+		/* add total count of unique words if we added new one to tree.
+		 * This will be useful for creating size of array we will need
+		 * to sort later by number of word appearances */
+		if(nw)
+		{
+			uniquewcnt++;
+			nw = 0;
+		}
+	}
+
+	/* create a flat array that qsort can work with (it can't work with a
+	 * binary tree */
+	struct wnode *wnodearr[uniquewcnt];
+	fillwnodelist(wnodearr, uniquewcnt);
+
+	qsort_r(wnodearr, sizeof wnodearr / sizeof *wnodearr, sizeof *wnodearr,
+			wcntcmp);
+
+	/* print the results */
+	/* pointer way of doing it */
+	struct wnode *wnodeend, *wnodeptr;
+	wnodeptr = wnodearr;
+	wnodeend = wnodearr + uniquewcnt; /* one past the end */
+
+	for( ; wnodeptr < wnodeend; wnodeptr++)
+		printf("%d,\t%s\n", wnodeptr->count, wnodeptr->word);
+
+	/* array index way of doing it */
+	/*int i;
+	for(i = 0; i < uniquewcnt; i++)
+		printf("%d,\t%s\n", wnodearr->count, wnodearr->word);*/
 }
 
 /* addword: add word to binary tree if not already present and update word 
